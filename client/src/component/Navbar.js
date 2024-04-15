df52dc443383857b3e062a52a4f7c713da7c9c8c
adarshawasthi29 <adarshawasthi127@gmail.com>
2024-04-15 23:48:34 +0530

    Name changed

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -21,7 +21,7 @@ function Navbar() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CallSpy</span>
           </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             {isLoggedIn ? (
